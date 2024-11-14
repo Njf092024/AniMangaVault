aaee93bef8b3797d1daad75650154ae23eb7f464
Njf092024 <Njf092024@gmail.com>
2024-11-14 13:10:20 +0100

    Add menu options for Anime/Manga management (Add, List, Update, Delete)

--- Program.cs
+++ Program.cs
@@ -9,6 +9,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        AnimeMangaService service = new AnimeMangaService();
+        int idCounter = 1;
+        bool exit = false;
         
+        while (!exit)
+        {
+            Console.Clear();
+            var prompt = new SelectionPrompt<string>()
+            .Title("[bold yellow]Anime/Manga Menu[/]")
+            .PageSize(10)
+            .AddChoices("Add new Anime/Manga", "List all Anime/Manga", "Update Rating", "Delete Anime/Manga", "Exit");
+
+            var selectedOption = AnsiConsole.Prompt(prompt);
+        }
     }
 }
