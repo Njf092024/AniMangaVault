8e12af543b8b7e6e2c62bc9fa27aba2412784bf9
Njf092024 <Njf092024@gmail.com>
2024-11-14 10:07:30 +0100

    add public string Description with get; set;

--- AnimeMangaItem.cs/AnimeMangaItem.cs
+++ AnimeMangaItem.cs/AnimeMangaItem.cs
@@ -3,4 +3,5 @@ public class AnimeMangaItem
     public int Id { get; set; }
     public string Title { get; set; }
     public string Type { get; set; }
+    public string Description { get; set; }
 }
